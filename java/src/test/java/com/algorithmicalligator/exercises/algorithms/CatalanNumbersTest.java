7645e6a3bc205c4528340d59eae9cc87fd57dfd6
Rob Bailes <RobBailes@blueyonder.co.uk>
2025-09-18 12:41:54 +0100

    Fixup the tests Q wrote

--- java/src/test/java/com/algorithmicalligator/exercises/algorithms/CatalanNumbersTest.java
+++ java/src/test/java/com/algorithmicalligator/exercises/algorithms/CatalanNumbersTest.java
@@ -6,11 +6,28 @@ import static org.junit.jupiter.api.Assertions.*;
 class CatalanNumbersTest {
     
     @Test
-    void testCatalanNumbers() {
+    void shouldReturn5_whenAskedForThe3rdCatalanNumber() {
         assertEquals(5, CatalanNumbers.catalanNumber(3));
+    }
+    
+    @Test
+    void shouldReturn14_whenAskedForThe4thCatalanNumber() {
         assertEquals(14, CatalanNumbers.catalanNumber(4));
+    }
+    
+    @Test
+    void shouldReturn132_whenAskedForThe6thCatalanNumber() {
         assertEquals(132, CatalanNumbers.catalanNumber(6));
+    }
+    
+    @Test
+    void shouldReturn42_whenAskedForThe5thCatalanNumber() {
+        // This should be cached, but checing that will require some mocking/spying
         assertEquals(42, CatalanNumbers.catalanNumber(5));
+    }
+    
+    @Test
+    void shouldReturn16796_whenAskedForThe10thCatalanNumber() {
         assertEquals(16796, CatalanNumbers.catalanNumber(10));
     }
 }
--- java/src/test/java/com/algorithmicalligator/exercises/algorithms/CoinSelectionTest.java
+++ java/src/test/java/com/algorithmicalligator/exercises/algorithms/CoinSelectionTest.java
@@ -7,11 +7,27 @@ import java.util.List;
 class CoinSelectionTest {
     
     @Test
-    void testCoinChange() {
+    void shouldUse2Coins_whenMaking4FromOnesTwosAndThrees() {
         assertEquals(2, CoinSelection.coinChange(4, List.of(1, 2, 3)));
+    }
+    
+    @Test
+    void shouldUse1Coin_whenTotalMatchesHighestDenomination() {
         assertEquals(1, CoinSelection.coinChange(5, List.of(1, 2, 5)));
+   }
+    
+    @Test
+    void shouldUseTwoFives_whenMaking10FromTwosThreesFivesAndSixes() {
         assertEquals(2, CoinSelection.coinChange(10, List.of(2, 5, 3, 6)));
-        assertEquals(1, CoinSelection.coinChange(3, List.of(1, 2, 3)));
+    }
+    
+    @Test
+    void shouldUse1Coin_whenTotalMatchesADenomination() {
+        assertEquals(1, CoinSelection.coinChange(3, List.of(1, 2, 3, 5)));
+    }
+    
+    @Test
+    void shouldReturnMinusOne_whenTotalCanNotBeMadeFromDenominations() {
         assertEquals(-1, CoinSelection.coinChange(7, List.of(2, 6)));
     }
 }
